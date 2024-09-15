76a4ac2933188d024fccba5d0abcef45b08ce36b
nisim <nisimohana44@gmail.com>
2024-09-15 17:20:48 +0300

    JTW added

--- 4-natours/starter/Controllers/AuthController.js
+++ 4-natours/starter/Controllers/AuthController.js
@@ -2,6 +2,11 @@ const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync.js');
 const appError = require('./../utils/appError.js');
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -10,10 +15,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfrim: req.body.passwordConfrim,
   });
-
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  const token = signToken(newUser._id);
 
   res.status(201).json({
     status: 'success',
@@ -24,7 +26,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.login = catchAsync((req, res, next) => {
+exports.login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
 
   //1) check if email and password exisit
@@ -33,9 +35,14 @@ exports.login = catchAsync((req, res, next) => {
   }
 
   //2)check if user exists && password is correct
-  const user = User.findOne({ email }); // style es6 for email: email(email ===email)
+  const user = await User.findOne({ email }).select('+password'); // style es6 for email: email(email ===email)
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new appError('incorrect email or password', 401));
+  }
+
   //3)if everything ok, send token to client
-  const token = '';
+  const token = signToken(user._id);
   res.status(200).json({
     status: 'success',
     token,
