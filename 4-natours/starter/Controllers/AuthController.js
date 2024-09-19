8e60eee6a77fea12fa10d9aab4971644e63bca2f
nisim <nisimohana44@gmail.com>
2024-09-19 14:28:31 +0300

    Updated regular data

--- 4-natours/starter/Controllers/AuthController.js
+++ 4-natours/starter/Controllers/AuthController.js
@@ -12,6 +12,18 @@ const signToken = (id) => {
   });
 };
 
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
+  });
+};
+
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
@@ -23,15 +35,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordResetToken: req.body.passwordResetToken,
     passwordResetExpires: req.body.passwordResetExpires,
   });
-  const token = signToken(newUser._id);
-
-  res.status(201).json({
-    status: 'success',
-    token,
-    data: {
-      user: newUser,
-    },
-  });
+  createSendToken(newUser, 201, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -50,11 +54,12 @@ exports.login = catchAsync(async (req, res, next) => {
   }
 
   //3)if everything ok, send token to client
-  const token = signToken(user._id);
-  res.status(200).json({
-    status: 'success',
-    token,
-  });
+  createSendToken(user, 200, res);
+  // const token = signToken(user._id);
+  // res.status(200).json({
+  //   status: 'success',
+  //   token,
+  // });
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
@@ -173,12 +178,29 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   //3)Update ChangePasswordAt proprety for the user
 
   //4)Log the user in, send JWT
-  const token = signToken(user._id);
+  createSendToken(user, 200, res);
 
-  res.status(201).json({
-    status: 'success',
-    token,
-  });
+  // const token = signToken(user._id);
+
+  // res.status(201).json({
+  //   status: 'success',
+  //   token,
+  // });
 });
 
-exports.updatePassword(req, res, next)=>{};
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  //1) get the user from the collection
+  const user = await User.findById(req.user.id).select('+password');
+  //2)Check if posted password is correct
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    //----
+    return next(new appError('Your currect Password is wrong', 401));
+  }
+  //3)if so, update password
+  user.password = req.body.password;
+  user.passwordConfrim = req.body.passwordConfrim;
+  await user.save();
+  //User.findByIdAndUpdate will NOT work as intended!
+  //4)Log user in, send JWT
+  createSendToken(user, 200, res);
+});
--- 4-natours/starter/Controllers/userController.js
+++ 4-natours/starter/Controllers/userController.js
@@ -1,8 +1,18 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('../utils/appError');
 
+const filterObj = (obj, ...allowFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowFields.includes(el)) {
+      newObj[el] = obj[el];
+    }
+  });
+  return newObj;
+};
 /// 2) ROUTE HANDLARES
-exports.getAllUsers = catchAsync(async (req, res, next) => {
+(exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
 
   res.status(200).json({
@@ -12,7 +22,37 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
       users,
     },
   });
-});
+})),
+  (exports.updateMe = catchAsync(async (req, res, next) => {
+    //1) create Error if user POSTs Password data
+    if (req.body.password || req.body.passwordConfrim) {
+      return next(
+        new AppError(
+          'This route is not for password updates, please use /updateMyPassword',
+          400,
+        ),
+      );
+    }
+
+    //3)filtered out unwanted fiels names that not allowed to be updated
+    const filteredBody = filterObj(req.body, 'name', 'email');
+    //3)Update user document
+    const updatedUser = await User.findByIdAndUpdate(
+      req.user.id,
+      filteredBody,
+      {
+        new: true,
+        runValidators: true,
+      },
+    );
+
+    res.status(200).json({
+      status: 'sucesss',
+      data: {
+        user: updatedUser,
+      },
+    });
+  }));
 
 exports.getUser = (req, res) => {
   res.status(500).json({
